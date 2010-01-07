949461b34c10a1bba4e4e4ec2c9fcb93994fe423
Sebastian Rockel <sebastianrockel@gmail.com>
2010-01-07 16:04:55 +0100

    Added opencv camera header files.

---
+++ include/cc_ballfinder.h
@@ -0,0 +1,160 @@
+#ifndef _CC_BALLFINDER_H_
+#define _CC_BALLFINDER_H_
+
+#include <cxcore.h>
+#include <cv.h>
+#include <highgui.h>
+
+const double cc_pi=3.14159265;
+
+struct Ball
+{
+  int num;
+  double* dist;
+  double* angle;
+};
+
+class BallFinder
+{
+  public:
+    int Init(int width,int height)
+    {
+      cx=705;
+      cy=492;
+      min_radius=3;
+      this->width=width;
+      this->height=height;
+      srcImage=cvCreateImage(cvSize(width,height),IPL_DEPTH_8U,3);
+      fltImage=cvCreateImage(cvGetSize(srcImage),8,1);
+      smImage=cvCreateImage(cvGetSize(srcImage),8,1);
+      hsv=cvCreateImage(cvGetSize(srcImage),8,3);
+      imageCircles=cvCreateImage(cvGetSize(srcImage),8,3);
+      imageBlobs=cvCreateImage(cvGetSize(srcImage),8,1);
+      cvNamedWindow("openCVwindow",CV_WINDOW_AUTOSIZE);
+      return 0;
+    }
+
+    Ball* DetectBall(unsigned char *img)
+    {
+      CvSeq* contour;
+      CvMemStorage* storBlob=cvCreateMemStorage(0);
+      CvRect rect;
+      Ball* bs;
+      int bx,by,br;
+      double angle;
+
+      YUV422toBGR(img,srcImage);
+      cvCvtColor(srcImage,hsv,CV_BGR2HSV);
+      cvInRangeS(hsv,cvScalar(140,140,30,0),cvScalar(171,256,256,0),fltImage);
+      cvSmooth(fltImage,smImage,CV_MEDIAN,3,3);
+//      cvSmooth(fltImage,smImage,CV_MEDIAN,9,9);
+      cvFindContours(smImage,storBlob,&contour,sizeof(CvContour),CV_RETR_EXTERNAL,CV_CHAIN_APPROX_SIMPLE);
+
+      bx=by=br=0;
+
+      for(;contour!=0;contour=contour->h_next)
+      {
+        rect=((CvContour*)contour)->rect;
+        if (rect.width<min_radius||rect.height<min_radius) continue;
+        // create an image with only this segment
+        cvZero(imageBlobs);
+        cvDrawContours(imageBlobs,contour,CV_RGB(255,255,255),CV_RGB(255,255,255),-1,CV_FILLED,8);
+        // Hough transform this blob
+        CvSeq* circles=cvHoughCircles(imageBlobs,storBlob,CV_HOUGH_GRADIENT,2,imageBlobs->height/4,200,20);
+        // if a circle was found
+        if (0<circles->total)
+        {
+          // keep only the largest circle
+          float* p=(float*)cvGetSeqElem(circles,0);
+          if (p[2]>br)
+          {
+            bx=p[0];
+            by=p[1];
+            br=p[2];
+          }
+        }
+        // draw this segment onto the output image with a random color
+      }
+
+      bs=new Ball;
+      bs->num=1;
+      if (br<=0)
+      {
+        cvShowImage("openCVwindow",srcImage);
+        bs->num=0;
+        return bs;
+      }
+      bs->dist=new double[bs->num];
+      bs->angle=new double[bs->num];
+
+      cvCircle(srcImage,cvPoint(bx,by),br,CV_RGB(255,0,0),3);
+      cvLine(srcImage,cvPoint(cx,cy),cvPoint(bx,by),CV_RGB(255,0,0),3);
+      angle=atan2(bx-cx,by-cy);
+      //cvEllipse(srcImage,cvPoint(cx,cy),cvSize(20,20),angle/pi*360,270,270-angle/pi*360,CV_RGB(255,0,0),2);
+      bs->angle[0]=angle;
+      bs->dist[0]=(by-cy)*(by-cy)+(bx-cx)*(bx-cx);
+
+      cvShowImage("openCVwindow",srcImage);
+      cvReleaseMemStorage(&storBlob);
+      return bs;
+    }
+
+    int Over()
+    {
+      cvDestroyWindow("openCVwindow");
+      cvReleaseImage(&fltImage);
+      cvReleaseImage(&smImage);
+      cvReleaseImage(&hsv);
+      cvReleaseImage(&imageCircles);
+      cvReleaseImage(&srcImage);
+      cvReleaseImage(&imageBlobs);
+      return 0;
+    }
+
+    bool IsContinue()
+    {
+      if (cvWaitKey(10)==1048603) return false;
+      return true;
+    }
+
+  private:
+    int width;
+    int height;
+    IplImage *srcImage;
+    IplImage* fltImage;
+    IplImage* smImage;
+    IplImage* hsv;
+    IplImage* imageCircles;
+    IplImage* imageBlobs;
+
+    int cx; // the x center of omni-image
+    int cy; // the y center of omni-image
+    int min_radius; // the ball smaller than this size will be ignored
+
+    void YUV422toBGR(unsigned char *src,IplImage *img)
+    {
+      int i,j,k,u,y1,v,y2;
+      // Top-Bottom, Left-Right
+      for (i=0;i<height;i++)
+      {
+        for (j=0;j<width;j+=2)
+        {
+          k=(i*width+j);
+          v=src[k*2];
+          y1=src[k*2+1];
+          u=src[k*2+2];
+          y2=src[k*2+3];
+          img->imageData[k*3]=y1;
+          img->imageData[k*3+1]=u;
+          img->imageData[k*3+2]=v;
+          img->imageData[k*3+3]=y2;
+          img->imageData[k*3+4]=u;
+          img->imageData[k*3+5]=v;
+        }
+      }
+      cvCvtColor(img,img,CV_YCrCb2BGR);
+    }
+};
+
+#endif
+
---
+++ include/cc_camera1394.h
@@ -0,0 +1,164 @@
+#ifndef _CC_CAMERA1394_H_
+#define _CC_CAMERA1394_H_
+
+#include <libraw1394/raw1394.h>
+#include <libdc1394/dc1394_control.h>
+
+typedef enum
+{
+  AF_off=0,
+  AF_global,
+  AF_locIni,
+  AF_local
+} focusModeTy;
+
+typedef enum
+{
+  low=0,
+  mid,
+  high
+} focusITy;
+
+typedef struct
+{
+  int val,sum;
+} focusTy;
+
+#define FOCUS_MIN 0
+#define FOCUS_MAX 447
+
+class Single1394
+{
+  public:
+    unsigned char* captureBuf;
+    int initCam(int width,int height)
+    {
+      int fwNodeNum,fwCamNum;
+      nodeid_t *fwCamNodes;
+      unsigned int speed;
+
+      this->width=width;
+      this->height=height;
+      imagelen=width*height*2;
+      captureBuf=(unsigned char *)malloc(imagelen);
+      // Create DC1394 handle
+      if ((fwHandle=dc1394_create_handle(0))==NULL)
+      {
+        printf("-E- failed to get raw1394 handle\n");
+        return 0;
+      }
+      // Get DC1394 node
+      fwNodeNum=raw1394_get_nodecount(fwHandle);
+      fwCamNodes=dc1394_get_camera_nodes(fwHandle,&fwCamNum,1);
+      if (fwCamNum<1)
+      {
+        printf("-E- no camera found\n");
+        dc1394_free_camera_nodes(fwCamNodes);
+        dc1394_destroy_handle(fwHandle);
+        return 0;
+      }
+      if (fwCamNum>1)
+      {
+        printf("-E- too many cameras: %i\n", fwCamNum);
+        dc1394_free_camera_nodes(fwCamNodes);
+        dc1394_destroy_handle(fwHandle);
+        return 0;
+      }
+      fwCamera.node = fwCamNodes[0];
+      // Get iso channel
+      if (dc1394_get_iso_channel_and_speed(fwHandle,fwCamera.node,(unsigned int *)&fwCamera.channel,(unsigned int *)&speed)!=DC1394_SUCCESS)
+      {
+        printf("-E- unable to get the iso channel number\n");
+        dc1394_free_camera_nodes(fwCamNodes);
+        dc1394_destroy_handle(fwHandle);
+        return 0;
+      }
+#ifdef INFO
+      // Get feature set
+      if (dc1394_get_camera_feature_set(fwHandle,fwCamera.node,&camFeatures)!=DC1394_SUCCESS) printf("-W- unable to get camera feature set\n");
+      else dc1394_print_feature_set(&camFeatures);
+#endif
+      // Camera dma setup
+      fwCamera.num_dma_buffers=1;
+      fwCamera.drop_frames=0;
+      fwCamera.dma_device_file=NULL;
+      if (width!=1280 || height!=960) printf("Currently the resolution is only supported by 1280x960!\nBe careful...\n");
+      if (dc1394_dma_setup_capture(
+	fwHandle,
+	fwCamera.node,
+	fwCamera.channel,
+	FORMAT_SVGA_NONCOMPRESSED_2,
+	MODE_1280x960_YUV422,SPEED_400,
+	FRAMERATE_7_5,
+	fwCamera.num_dma_buffers,
+	fwCamera.drop_frames,
+	fwCamera.dma_device_file,
+	&fwCamera)!=DC1394_SUCCESS) 
+      {
+        printf("-E- unable to setup camera; check line %d of %s\n",__LINE__,__FILE__);
+        dc1394_free_camera_nodes(fwCamNodes);
+        dc1394_destroy_handle(fwHandle);
+        return 0;
+      }
+      // Start DC1394 iso trasmission
+      if (dc1394_start_iso_transmission(fwHandle,fwCamera.node)!=DC1394_SUCCESS)
+      {
+        printf("-E- unable to start camera iso transmission\n");
+        dc1394_free_camera_nodes(fwCamNodes);
+        return 0;
+      }
+      dc1394_free_camera_nodes(fwCamNodes);
+      return 1;
+    }
+
+    void initFocus()
+    {
+      int focRange;
+      printf("-I- AF: on     - global");
+      focusCnt=0;
+      focusMode=AF_global;
+      focusI=low;
+      focusLo=FOCUS_MIN;
+      focusHi=FOCUS_MAX;
+      focRange=focusHi-focusLo+1;
+      focus[low].val=focRange/4;
+      focus[mid].val=focRange/2;
+      focus[high].val=focRange*3/4;
+      dc1394_set_focus(fwHandle, fwCamera.node, focus[mid].val); // now the focus is set to MID
+      printf("\n");
+    }
+
+    void cleanup()
+    {
+      free(captureBuf);
+
+      dc1394_stop_iso_transmission(fwHandle,fwCamera.node);
+      dc1394_dma_unlisten(fwHandle,&fwCamera);
+      dc1394_dma_release_camera(fwHandle,&fwCamera);
+      dc1394_destroy_handle(fwHandle);
+    }
+
+    int captureImage()
+    {
+      dc1394_dma_single_capture(&fwCamera);
+      dc1394_dma_done_with_buffer(&fwCamera);
+      memcpy(captureBuf,fwCamera.capture_buffer,imagelen);
+      return 1;
+    }
+
+  private:
+    raw1394handle_t fwHandle;
+    dc1394_cameracapture fwCamera;
+
+    focusTy focus[3];
+    unsigned int focusCnt;
+    focusModeTy focusMode;
+    focusITy focusI;
+    int focusLo,focusHi;
+
+    int width;
+    int height;
+    int imagelen;
+};
+
+#endif
